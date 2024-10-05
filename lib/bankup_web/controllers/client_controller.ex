720e330b628880851b9fd3f5cf34ac5ed18f3b81
Vitória Moura <vim.saturnino@gmail.com>
2024-10-05 16:34:21 -0300

    feat: implement ClientController for CRUD operations

--- lib/bankup_web/controllers/client_controller.ex
+++ lib/bankup_web/controllers/client_controller.ex
@@ -8,28 +8,28 @@ defmodule BankupWeb.ClientController do
 
   def index(conn, _params) do
     clients = Clients.list_clients()
-    render(conn, "index.json", clients: clients)
+    json(conn, %{data: clients})
   end
 
   def create(conn, %{"client" => client_params}) do
     with {:ok, %Client{} = client} <- Clients.create_client(client_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.client_path(conn, :show, client))
-      |> render("show.json", client: client)
+      |> put_resp_header("location", ~p"/api/clients/#{client.id}")
+      |> json(%{data: client})
     end
   end
 
   def show(conn, %{"id" => id}) do
     client = Clients.get_client!(id)
-    render(conn, "show.json", client: client)
+    json(conn, %{data: client})
   end
 
   def update(conn, %{"id" => id, "client" => client_params}) do
     client = Clients.get_client!(id)
 
     with {:ok, %Client{} = client} <- Clients.update_client(client, client_params) do
-      render(conn, "show.json", client: client)
+      json(conn, %{data: client})
     end
   end
 
