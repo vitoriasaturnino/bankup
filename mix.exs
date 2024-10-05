feccf212c873c850b039946f9ec9fc6466d348ed
Vitória Moura <vim.saturnino@gmail.com>
2024-10-05 17:29:02 -0300

    fix: add Mox as a test-only dependency for mocks in tests

--- mix.exs
+++ mix.exs
@@ -53,7 +53,8 @@ defmodule Bankup.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:mox, "~> 1.2"}
     ]
   end
 
