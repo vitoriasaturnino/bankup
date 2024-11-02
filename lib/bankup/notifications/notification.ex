8118771448597d1c656440b2d9ba26d8197cacbf
Vitória Moura <vim.saturnino@gmail.com>
2024-11-02 15:27:50 -0300

    feat: add notifications business features

--- lib/bankup/notifications/notification.ex
+++ lib/bankup/notifications/notification.ex
@@ -1,24 +1,26 @@
 defmodule Bankup.Notifications.Notification do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Bankup.Clients.Client
+  alias Bankup.RecurringAccounts.RecurringAccount
 
-  @primary_key {:id, :binary_id, autogenerate: true}
-  @foreign_key_type :binary_id
   schema "notifications" do
-    field :channel, :string
+    field :channel, :string, default: "ambos"
     field :content, :string
     field :sent_at, :utc_datetime
-    field :delivery_status, :string
-    field :client_id, :binary_id
-    field :account_id, :binary_id
+    field :delivery_status, :string, default: "pendente"
+    belongs_to :client, Client
+    belongs_to :recurring_account, RecurringAccount
 
-    timestamps(type: :utc_datetime)
+    timestamps()
   end
 
   @doc false
   def changeset(notification, attrs) do
     notification
-    |> cast(attrs, [:channel, :content, :sent_at, :delivery_status])
-    |> validate_required([:channel, :content, :sent_at, :delivery_status])
+    |> cast(attrs, [:client_id, :account_id, :channel, :content, :sent_at, :delivery_status])
+    |> validate_required([:client_id, :account_id, :channel, :content, :delivery_status])
+    |> assoc_constraint(:client)
+    |> assoc_constraint(:recurring_account)
   end
 end
