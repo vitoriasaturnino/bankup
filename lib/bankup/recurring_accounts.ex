42928e31d4ef9b5e2ff96bcb37932364b480cd08
Vitória Moura <vim.saturnino@gmail.com>
2024-11-02 15:25:50 -0300

    feat: add recurring_accounts business features

--- lib/bankup/recurring_accounts.ex
+++ lib/bankup/recurring_accounts.ex
@@ -1,53 +1,11 @@
 defmodule Bankup.RecurringAccounts do
-  @moduledoc """
-  The RecurringAccounts context.
-  """
+  use Ecto.Schema
 
-  import Ecto.Query, warn: false
   alias Bankup.Repo
-
   alias Bankup.RecurringAccounts.RecurringAccount
 
   @doc """
-  Returns the list of recurring_accounts.
-
-  ## Examples
-
-      iex> list_recurring_accounts()
-      [%RecurringAccount{}, ...]
-
-  """
-  def list_recurring_accounts do
-    Repo.all(RecurringAccount)
-  end
-
-  @doc """
-  Gets a single recurring_account.
-
-  Raises `Ecto.NoResultsError` if the Recurring account does not exist.
-
-  ## Examples
-
-      iex> get_recurring_account!(123)
-      %RecurringAccount{}
-
-      iex> get_recurring_account!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_recurring_account!(id), do: Repo.get!(RecurringAccount, id)
-
-  @doc """
-  Creates a recurring_account.
-
-  ## Examples
-
-      iex> create_recurring_account(%{field: value})
-      {:ok, %RecurringAccount{}}
-
-      iex> create_recurring_account(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
+  Cria uma nova conta recorrente associada a um cliente.
   """
   def create_recurring_account(attrs \\ %{}) do
     %RecurringAccount{}
@@ -56,49 +14,18 @@ defmodule Bankup.RecurringAccounts do
   end
 
   @doc """
-  Updates a recurring_account.
-
-  ## Examples
-
-      iex> update_recurring_account(recurring_account, %{field: new_value})
-      {:ok, %RecurringAccount{}}
-
-      iex> update_recurring_account(recurring_account, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
+  Atualiza uma conta recorrente existente.
   """
-  def update_recurring_account(%RecurringAccount{} = recurring_account, attrs) do
-    recurring_account
+  def update_recurring_account(%RecurringAccount{} = account, attrs) do
+    account
     |> RecurringAccount.changeset(attrs)
     |> Repo.update()
   end
 
   @doc """
-  Deletes a recurring_account.
-
-  ## Examples
-
-      iex> delete_recurring_account(recurring_account)
-      {:ok, %RecurringAccount{}}
-
-      iex> delete_recurring_account(recurring_account)
-      {:error, %Ecto.Changeset{}}
-
+  Inativa uma conta recorrente, alterando o status para "inativa".
   """
-  def delete_recurring_account(%RecurringAccount{} = recurring_account) do
-    Repo.delete(recurring_account)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking recurring_account changes.
-
-  ## Examples
-
-      iex> change_recurring_account(recurring_account)
-      %Ecto.Changeset{data: %RecurringAccount{}}
-
-  """
-  def change_recurring_account(%RecurringAccount{} = recurring_account, attrs \\ %{}) do
-    RecurringAccount.changeset(recurring_account, attrs)
+  def deactivate_recurring_account(%RecurringAccount{} = account) do
+    update_recurring_account(account, %{status: "inativa"})
   end
 end
--- lib/bankup/recurring_accounts/recurring_account.ex
+++ lib/bankup/recurring_accounts/recurring_account.ex
@@ -1,25 +1,26 @@
 defmodule Bankup.RecurringAccounts.RecurringAccount do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Bankup.Clients.Client
 
-  @primary_key {:id, :binary_id, autogenerate: true}
-  @foreign_key_type :binary_id
   schema "recurring_accounts" do
-    field :status, :string
     field :description, :string
     field :amount, :integer
     field :due_date, :date
     field :payee, :string
     field :pix_key, :string
-    field :client_id, :binary_id
+    field :status, :string, default: "ativa"
+    belongs_to :client, Client
 
-    timestamps(type: :utc_datetime)
+    timestamps()
   end
 
   @doc false
   def changeset(recurring_account, attrs) do
     recurring_account
-    |> cast(attrs, [:description, :amount, :due_date, :payee, :pix_key, :status])
-    |> validate_required([:description, :amount, :due_date, :payee, :pix_key, :status])
+    |> cast(attrs, [:description, :amount, :due_date, :payee, :pix_key, :status, :client_id])
+    |> validate_required([:description, :amount, :due_date, :payee, :status, :client_id])
+    |> validate_number(:amount, greater_than: 0)
+    |> assoc_constraint(:client)
   end
 end
